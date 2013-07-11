12730359462475be6f3e91c99ecc4bf9f3915cf3
jsk <knomedia@gmail.com>
2013-07-11 10:06:09 -0600

    Add shell script to update vendor assets in the future

---
+++ update_from_vendor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Checkout vendor repo
+echo "Cloning vendor repo into tmp_vendor"
+git clone https://github.com/nostalgiaz/bootstrap-switch.git tmp_vendor
+
+# Copy files
+echo "Copying bootstrap-switch.js"
+cp tmp_vendor/static/js/bootstrap-switch.js vendor/assets/javascripts/bootstrap-switch.js
+echo "Copying bootstrap-switch.css"
+cp tmp_vendor/static/stylesheets/bootstrap-switch.css vendor/assets/stylesheets/bootstrap-switch.css
+
+# Delete vendor repo
+echo "Removing cloned vendor repo"
+rm -rf tmp_vendor
+
+echo "Finished... You'll need to commit the changes. You should consider updating the changelog and gem version number"
