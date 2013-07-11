d13b7bc86667ffa5f764cdbd162b2588684a9eae
jsk <knomedia@gmail.com>
2013-07-11 10:09:10 -0600

    List github vendor repo in shell output

--- update_from_vendor.sh
+++ update_from_vendor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Checkout vendor repo
-echo "Cloning vendor repo into tmp_vendor"
+echo "Cloning nostalgiaz/bootstrap-switch github repo into tmp_vendor"
 git clone https://github.com/nostalgiaz/bootstrap-switch.git tmp_vendor
 
 # Copy files
