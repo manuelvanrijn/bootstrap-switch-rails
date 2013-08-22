fcfe569f3b49adb4b936eff3a5869736a699702c
Manuel van Rijn <vanrijn@auxilium.nl>
2013-08-22 12:28:05 +0200

    .css to .css.scss: easier to import in a sass file & update update_fron_vendor.sh file

--- update_from_vendor.sh
+++ update_from_vendor.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Checkout vendor repo
 echo "Cloning nostalgiaz/bootstrap-switch github repo into tmp_vendor"
@@ -8,7 +8,7 @@ git clone https://github.com/nostalgiaz/bootstrap-switch.git tmp_vendor
 echo "Copying bootstrap-switch.js"
 cp tmp_vendor/static/js/bootstrap-switch.js vendor/assets/javascripts/bootstrap-switch.js
 echo "Copying bootstrap-switch.css"
-cp tmp_vendor/static/stylesheets/bootstrap-switch.css vendor/assets/stylesheets/bootstrap-switch.css
+cp tmp_vendor/static/stylesheets/bootstrap-switch.css vendor/assets/stylesheets/bootstrap-switch.css.scss
 
 # Delete vendor repo
 echo "Removing cloned vendor repo"
